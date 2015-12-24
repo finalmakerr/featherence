3458e178978832a124e009fbd2258233b2d95330
finalmakerr <finalmakerr@gmail.com>
2015-12-24 20:36:01 +0200

    Featherence Music v0.1.5 (24-12-15)
    - Added Great French Collection Amir's playlists.
    - Added The Beatles
    Amir's playlists.

--- master/plugin.video.featherence.music/modules.py
+++ master/plugin.video.featherence.music/modules.py
@@ -53,7 +53,7 @@ def CATEGORIES100(admin):
 
 	
 def CATEGORIES10001(name, iconimage, desc, fanart):
-	
+	if desc == None: desc = ""
 	'''Easy Listening, Love songs & Bellads'''
 	thumb = 'https://d85wutc1n854v.cloudfront.net/live/products/600x375/WB0PGGM81.png?v=1.0'
 	fanart = 'http://www.pavellevchenko.com/hard-rock-music-guitar.jpg'
