06bdab4bbddf203b0f7654884bf96763ff8b678f
finalmakerr@gmail.com <finalmakerr@gmail.com>
2016-05-21 21:38:30 +0300

    !

---
+++ master/emulator.retroarch/default.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import xbmc, xbmcgui, xbmcaddon, os, sys, subprocess, random
+
+pluginprogramfeatherenceemuPath          = xbmcaddon.Addon('plugin.program.featherence.emu').getAddonInfo("path")
+sharedlibDir2 = os.path.join(pluginprogramfeatherenceemuPath, '')
+sys.path.insert(0, sharedlibDir2)
+
+from variables import *
+from modules import *
+
+if systemplatformlinux:
+	fn = dialog.browse(1, 'retroarch', 'files')
+	os.system( "chmod a+rx " + emulator_file )
+	os.system( "%s '%s' "%(emulator_file,fn.replace("'", "'\\''")) )
+
+elif systemplatformwindows:
+	os.system("start " + emulator_file + space + launcher_args + space + '--menu')
