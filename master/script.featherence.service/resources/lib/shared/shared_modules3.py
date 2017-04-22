a805d78b890ba1a1a5cf413c147e39b427465681
finalmakerr <finalmakerr@gmail.com>
2017-04-22 20:09:02 +0300

    Revert "!"
    This reverts commit 3ff591a9921901515ad9df77da338e882e3bf6b6.

--- master/script.featherence.service/resources/lib/shared/shared_modules3.py
+++ master/script.featherence.service/resources/lib/shared/shared_modules3.py
@@ -2043,7 +2043,6 @@ def pluginend(admin):
 					if 'Hebrew' in General_LanguageL:
 						installaddonP('repository.xbmc-israel', update=True)
 						#installaddonP('repository.kodil', update=True)
-						installaddonP('repository.multidownrepo', update=True)
 						installaddonP('plugin.video.jksp', update=True)
 				
 					if 'English' in General_LanguageL:
